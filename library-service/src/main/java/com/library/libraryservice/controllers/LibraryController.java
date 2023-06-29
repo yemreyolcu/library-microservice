f0ba6b44969db2eb18a43e9a88ed543aab6a1ef0
yemreyolcu <yemrewho@outlook.com>
2023-06-29 20:20:56 +0300

    db moved to psql from h2

--- library-service/src/main/java/com/library/libraryservice/controllers/LibraryController.java
+++ library-service/src/main/java/com/library/libraryservice/controllers/LibraryController.java
@@ -3,17 +3,19 @@ package com.library.libraryservice.controllers;
 import com.library.libraryservice.dtos.CreateBookDto;
 import com.library.libraryservice.dtos.LibraryDto;
 import com.library.libraryservice.services.LibraryService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/library")
 public class LibraryController {
     private final LibraryService libraryService;
+    @Value("${library.service.count}")
+    private int count;
 
     public LibraryController(LibraryService libraryService) {
         this.libraryService = libraryService;
@@ -34,5 +36,10 @@ public class LibraryController {
     public ResponseEntity<Void> addBookToLibrary(@RequestBody CreateBookDto request) {
         libraryService.addBookToLibrary(request);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount() {
+        return new ResponseEntity<>("Count: " + count, HttpStatus.OK);
     }
 }
