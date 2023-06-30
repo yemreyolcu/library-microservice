15635b035d270fc5fa0b149684544f22fd342fef
yemreyolcu <yemrewho@outlook.com>
2023-06-30 18:23:23 +0300

    flyway bug fixed

--- book-service/src/main/java/com/book/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/book/bookservice/BookServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class BookServiceApplication  {
 
-
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
     }
---
+++ book-service/src/main/resources/db.migration/V2__insert_books.sql
@@ -0,0 +1,13 @@
+INSERT INTO "book-service".public.book (id, title, author, isbn, press_name, book_year)
+VALUES ('e28e2028-340e-4bca-9e12-3dc60c31b460', 'The Lord of the Rings', 'J.R.R. Tolkien', '978-0618640157', 'LOTR',
+        1955);
+
+INSERT INTO "book-service".public.book (id, title, author, isbn, press_name, book_year)
+VALUES ('e28e2028-340e-4bca-9e12-3dc60c31b461', 'The Hobbit', 'J.R.R. Tolkien', '978-0547928227', 'HOBBIT', 1937);
+
+INSERT INTO "book-service".public.book (id, title, author, isbn, press_name, book_year)
+VALUES ('e28e2028-340e-4bca-9e12-3dc60c31b462', 'The Fellowship of the Ring', 'J.R.R. Tolkien', '978-0618346257',
+        'LOTR2', 1954);
+
+INSERT INTO "book-service".public.book (id, title, author, isbn, press_name, book_year)
+VALUES ('e28e2028-340e-4bca-9e12-3dc60c31b463', 'The Two Towers', 'J.R.R. Tolkien', '978-0618346264', 'LOTR3', 1954);
