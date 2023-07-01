7b7ae94458939119580a6cf132a6b7408747a067
yemreyolcu <yemrewho@outlook.com>
2023-07-01 17:02:17 +0300

    user microservice created and default values added with flyway

---
+++ book-service/src/main/resources/db.migration/V1__create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE public."book"
+(
+    id         VARCHAR(255) NOT NULL,
+    title      VARCHAR(255) NOT NULL,
+    author     VARCHAR(255) NOT NULL,
+    isbn       VARCHAR(255) NOT NULL,
+    press_name VARCHAR(255) NOT NULL,
+    book_year  INTEGER      NOT NULL,
+    CONSTRAINT pk_book PRIMARY KEY (id)
+);
